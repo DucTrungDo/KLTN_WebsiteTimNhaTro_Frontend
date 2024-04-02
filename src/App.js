f9be9ddd3cab641cc6a3550f1ab7b260629440c2
Trình Lê <lecongtrinhbhxh@gmail.com>
2024-04-02 22:43:40 +0700

    UI postDetail

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Home from './components/Home'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import UserDashboard from './components/user/Dashboard'
+import PostDetail from './components/post/PostDetail'
 
 function App() {
   return (
@@ -23,6 +24,8 @@ function App() {
               <Route path='/login' element={<Login />} />
               <Route path='/register' element={<Register />} />
               <Route path='/user/dashboard' element={<UserDashboard />} />
+
+              <Route path='/post_detail' element={<PostDetail />} />
             </Routes>
           </div>
         </main>
