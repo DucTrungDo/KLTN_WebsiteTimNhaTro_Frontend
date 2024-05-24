42afed21664a768a00aab6c9f99d28885499c9f8
DoTrungDuc <ducdt2192@gmail.com>
2024-05-24 12:19:58 +0700

    Edit status display PostManagement for user

--- src/components/user/PostManagement.js
+++ src/components/user/PostManagement.js
@@ -411,130 +411,175 @@ const PostManagement = () => {
                           {post.address?.city}
                         </p>
                         <div className='post_btn_toolbar mt-3'>
-                          <a
-                            href='https://phongtro123.com/quan-ly/tin-dang/gia-han-tin/653234'
-                            className='btn btn-sm mt-2 btn_danglai btn-warning text-success'
-                          >
-                            <svg
-                              xmlns='http://www.w3.org/2000/svg'
-                              width='24'
-                              height='24'
-                              viewBox='0 0 24 24'
-                              fill='none'
-                              stroke='currentColor'
-                              strokeWidth={2}
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              className='feather feather-plus'
+                          {post.isPaid &&
+                            post.isApproved &&
+                            !post.isViolated &&
+                            !post.isHided && (
+                              <a
+                                href='https://phongtro123.com/quan-ly/tin-dang/gia-han-tin/653234'
+                                className='btn btn-sm mt-2 btn_danglai btn-warning text-success'
+                              >
+                                <svg
+                                  xmlns='http://www.w3.org/2000/svg'
+                                  width='24'
+                                  height='24'
+                                  viewBox='0 0 24 24'
+                                  fill='none'
+                                  stroke='currentColor'
+                                  strokeWidth={2}
+                                  strokeLinecap='round'
+                                  strokeLinejoin='round'
+                                  className='feather feather-plus'
+                                >
+                                  <line x1='12' y1='5' x2='12' y2='19'></line>
+                                  <line x1='5' y1='12' x2='19' y2='12'></line>
+                                </svg>
+                                Thêm ngày
+                              </a>
+                            )}
+
+                          {((post.isPaid && !post.isApproved) ||
+                            post.isHided ||
+                            post.isViolated) && (
+                            <a
+                              href='https://phongtro123.com/quan-ly/post/gan-nhan/653234'
+                              className='btn btn-sm mt-2'
                             >
-                              <line x1='12' y1='5' x2='12' y2='19'></line>
-                              <line x1='5' y1='12' x2='19' y2='12'></line>
-                            </svg>
-                            Thêm ngày
-                          </a>
-                          <a
-                            href='https://phongtro123.com/quan-ly/post/gan-nhan/653234'
-                            className='btn btn-sm mt-2'
-                          >
-                            <svg
-                              xmlns='http://www.w3.org/2000/svg'
-                              width='24'
-                              height='24'
-                              viewBox='0 0 24 24'
-                              fill='none'
-                              stroke='currentColor'
-                              strokeWidth={2}
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              className='feather feather-tag'
+                              <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                width='24'
+                                height='24'
+                                viewBox='0 0 24 24'
+                                fill='none'
+                                stroke='currentColor'
+                                strokeWidth={2}
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                class='feather feather-refresh-ccw'
+                              >
+                                <polyline points='1 4 1 10 7 10'></polyline>
+                                <polyline points='23 20 23 14 17 14'></polyline>
+                                <path d='M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15'></path>
+                              </svg>
+                              Sửa và đăng lại
+                            </a>
+                          )}
+                          {!post.isPaid && (
+                            <a
+                              href='https://phongtro123.com/quan-ly/post/gan-nhan/653234'
+                              className='btn btn-sm mt-2'
                             >
-                              <path d='M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z'></path>
-                              <line x1='7' y1='7' x2='7' y2='7'></line>
-                            </svg>
-                            Gắn nhãn
-                          </a>
-                          <Link
-                            to={'/user/post/edit/' + post.slug}
-                            className='btn btn-sm mt-2'
-                          >
-                            <svg
-                              xmlns='http://www.w3.org/2000/svg'
-                              width='24'
-                              height='24'
-                              viewBox='0 0 24 24'
-                              fill='none'
-                              stroke='currentColor'
-                              strokeWidth={2}
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              className='feather feather-edit-2'
-                            >
-                              <polygon points='16 3 21 8 8 21 3 21 3 16 16 3'></polygon>
-                            </svg>
-                            Sửa tin
-                          </Link>
-                          <a
-                            href='https://phongtro123.com/quan-ly/tin-dang/day-tin/653234'
-                            className='btn btn-sm mt-2'
-                          >
-                            <svg
-                              xmlns='http://www.w3.org/2000/svg'
-                              width='24'
-                              height='24'
-                              viewBox='0 0 24 24'
-                              fill='none'
-                              stroke='currentColor'
-                              strokeWidth={2}
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              className='feather feather-arrow-up'
-                            >
-                              <line x1='12' y1='19' x2='12' y2='5'></line>
-                              <polyline points='5 12 12 5 19 12'></polyline>
-                            </svg>
-                            Đẩy tin
-                          </a>
-                          <a
-                            href='https://phongtro123.com/quan-ly/tin-dang/nang-cap-tin/653234'
-                            className='btn btn-sm mt-2'
-                          >
-                            <svg
-                              xmlns='http://www.w3.org/2000/svg'
-                              width='24'
-                              height='24'
-                              viewBox='0 0 24 24'
-                              fill='none'
-                              stroke='currentColor'
-                              strokeWidth={2}
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              className='feather feather-star'
-                            >
-                              <polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2'></polygon>
-                            </svg>
-                            Nâng cấp VIP
-                          </a>
-                          <a
-                            href='https://phongtro123.com/quan-ly/tin-dang/an-tin/653234'
-                            className='btn btn-sm mt-2'
-                          >
-                            <svg
-                              xmlns='http://www.w3.org/2000/svg'
-                              width='24'
-                              height='24'
-                              viewBox='0 0 24 24'
-                              fill='none'
-                              stroke='currentColor'
-                              strokeWidth={2}
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              className='feather feather-eye-off'
-                            >
-                              <path d='M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24'></path>
-                              <line x1='1' y1='1' x2='23' y2='23'></line>
-                            </svg>
-                            Ẩn tin
-                          </a>
+                              <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                width='24'
+                                height='24'
+                                viewBox='0 0 24 24'
+                                fill='none'
+                                stroke='currentColor'
+                                strokeWidth={2}
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                className='feather feather-tag'
+                              >
+                                <path d='M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z'></path>
+                                <line x1='7' y1='7' x2='7' y2='7'></line>
+                              </svg>
+                              Thanh toán
+                            </a>
+                          )}
+
+                          {post.isPaid &&
+                            post.isApproved &&
+                            !post.isViolated &&
+                            !post.isHided && (
+                              <>
+                                <Link
+                                  to={'/user/post/edit/' + post.slug}
+                                  className='btn btn-sm mt-2'
+                                >
+                                  <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    width='24'
+                                    height='24'
+                                    viewBox='0 0 24 24'
+                                    fill='none'
+                                    stroke='currentColor'
+                                    strokeWidth={2}
+                                    strokeLinecap='round'
+                                    strokeLinejoin='round'
+                                    className='feather feather-edit-2'
+                                  >
+                                    <polygon points='16 3 21 8 8 21 3 21 3 16 16 3'></polygon>
+                                  </svg>
+                                  Sửa tin
+                                </Link>
+
+                                <a
+                                  href='https://phongtro123.com/quan-ly/tin-dang/day-tin/653234'
+                                  className='btn btn-sm mt-2'
+                                >
+                                  <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    width='24'
+                                    height='24'
+                                    viewBox='0 0 24 24'
+                                    fill='none'
+                                    stroke='currentColor'
+                                    strokeWidth={2}
+                                    strokeLinecap='round'
+                                    strokeLinejoin='round'
+                                    className='feather feather-arrow-up'
+                                  >
+                                    <line x1='12' y1='19' x2='12' y2='5'></line>
+                                    <polyline points='5 12 12 5 19 12'></polyline>
+                                  </svg>
+                                  Đẩy tin
+                                </a>
+
+                                <a
+                                  href='https://phongtro123.com/quan-ly/tin-dang/nang-cap-tin/653234'
+                                  className='btn btn-sm mt-2'
+                                >
+                                  <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    width='24'
+                                    height='24'
+                                    viewBox='0 0 24 24'
+                                    fill='none'
+                                    stroke='currentColor'
+                                    strokeWidth={2}
+                                    strokeLinecap='round'
+                                    strokeLinejoin='round'
+                                    className='feather feather-star'
+                                  >
+                                    <polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2'></polygon>
+                                  </svg>
+                                  Nâng cấp tin
+                                </a>
+
+                                <a
+                                  href='https://phongtro123.com/quan-ly/tin-dang/an-tin/653234'
+                                  className='btn btn-sm mt-2'
+                                >
+                                  <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    width='24'
+                                    height='24'
+                                    viewBox='0 0 24 24'
+                                    fill='none'
+                                    stroke='currentColor'
+                                    strokeWidth={2}
+                                    strokeLinecap='round'
+                                    strokeLinejoin='round'
+                                    className='feather feather-eye-off'
+                                  >
+                                    <path d='M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24'></path>
+                                    <line x1='1' y1='1' x2='23' y2='23'></line>
+                                  </svg>
+                                  Ẩn tin
+                                </a>
+                              </>
+                            )}
                           <button
                             className='btn btn-sm mt-2'
                             type='button'
@@ -590,20 +635,28 @@ const PostManagement = () => {
                       <td>23/04/2024 21:26:13</td>
                       <td>28/04/2024 21:26:13</td>
                       <td>
-                        <span
-                          className='text-success label-success'
+                        <div
+                          className='label-success'
                           style={{ whiteSpace: 'nowrap' }}
                         >
-                          {post.isHided
-                            ? 'Tin đã ẩn'
-                            : post.isViolated
-                            ? 'Tin bị cảnh báo'
-                            : !post.isPaid
-                            ? 'Tin chưa thanh toán'
-                            : !post.isApproved
-                            ? 'Tin chưa được duyệt'
-                            : 'Tin đã được duyệt'}
-                        </span>
+                          {post.isHided ? (
+                            <span className='text-warning'>Tin đã ẩn</span>
+                          ) : post.isViolated ? (
+                            <span className='text-danger'>Tin bị vi phạm</span>
+                          ) : !post.isPaid ? (
+                            <span className='text-info'>
+                              Tin chưa thanh toán
+                            </span>
+                          ) : !post.isApproved ? (
+                            <span className='text-primary'>
+                              Tin đang chờ duyệt
+                            </span>
+                          ) : (
+                            <span className='text-success'>
+                              Tin đã được duyệt
+                            </span>
+                          )}
+                        </div>
                       </td>
                     </tr>
                   ))
